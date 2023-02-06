ab2de64e1cee12a5140a877d54454258abc3cbe6
sweet <yuuu.shizhen@gmail.com>
2023-02-06 21:00:48 +0800

    修改数据库配置

--- service/user/internal/model/init.go
+++ service/user/internal/model/init.go
@@ -9,7 +9,7 @@ import (
 var DB *gorm.DB
 
 func NewDB() {
-	dsn := "root:shi@tcp(43.139.184.22:3306)/tiktok?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "sz:123456@tcp(43.139.184.22:3306)/tiktok?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn))
 	if err != nil {
 		log.Fatalln("[DB ERROR] : ", err)
